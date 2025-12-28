6a36425e3cab1b098dbb0cb4d14652703725474c
Priyanshi Sathwara <priyanshisathwara04@gmail.com>
2025-12-28 18:22:39 +0530

    Fix: build issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,28 @@
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 import Logo from "../assets/logo/Rajwadi png.png";
 
 const Navbar = () => {
+  const togglerRef = useRef(null);
+
+  const closeNavbar = () => {
+    if (
+      togglerRef.current &&
+      togglerRef.current.getAttribute("aria-expanded") === "true"
+    ) {
+      togglerRef.current.click();
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-        <img className="nav-logo" src={Logo} alt='asd'></img>
+        <Link className="navbar-brand" to="/" onClick={closeNavbar}>
+          <img className="nav-logo" src={Logo} alt="Logo" />
         </Link>
+
         <button
+          ref={togglerRef}
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -19,26 +33,22 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/franchise">Franchise</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About Us</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact Us</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/branches">Branches</Link>
-            </li>
-            {/* <li className="nav-item">
-              <Link className="nav-link" to="/careers">Careers</Link>
-            </li> */}
+            {[
+              { path: "/", label: "Home" },
+              { path: "/franchise", label: "Franchise" },
+              { path: "/about", label: "About Us" },
+              { path: "/contact", label: "Contact Us" },
+              { path: "/branches", label: "Branches" },
+            ].map(({ path, label }) => (
+              <li className="nav-item" key={path}>
+                <Link className="nav-link" to={path} onClick={closeNavbar}>
+                  {label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
