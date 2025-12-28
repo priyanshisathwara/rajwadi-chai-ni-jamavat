5b2f6e373e801a758077d9781e0853ee5a99e0cd
Priyanshi Sathwara <priyanshisathwara04@gmail.com>
2025-12-28 18:10:35 +0530

    Fix: build issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroSection from '../components/Herosection';
-import ContactPopup from '../components/Contactpopup';
+import ContactPopup from '../components/ContactPopup';
 import AboutUsSection from '../components/AboutUsSection';
 import ImageGrid from '../components/ImageGrid';
 import Branches from '../components/Branches';
