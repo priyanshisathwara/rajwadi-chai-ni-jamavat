7c4ebfe93a3264ee443cd99e508533ddae17c51d
Priyanshi Sathwara <priyanshisathwara04@gmail.com>
2026-01-04 21:09:25 +0530

    Adding google site verification file in public as verification code is not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -8,37 +7,28 @@ import Home from "./pages/Home";
 import Franchise from "./pages/Franchise.jsx";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import WhatsAppButton from '../src/components/WhatsAppButton.jsx';
+import WhatsAppButton from "./components/WhatsAppButton";
 import Branches from "./components/Branches.jsx";
 
 
 function App() {
   return (
-    <>
-      <Helmet>
-        <meta
-          name="google-site-verification"
-          content="1ezIJcPuQEe0vMdRw3ck8CYG65hZ8rsqF04TyqeQ7rs"
-        />
-      </Helmet>
+    <BrowserRouter>
+      <Navbar />
+      <WhatsAppButton showButton={false} />
 
-      <BrowserRouter>
-        <Navbar />
-        <WhatsAppButton showButton={false} />
+      <main style={{ minHeight: "80vh" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/franchise" element={<Franchise />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/branches" element={<Branches />} />
+        </Routes>
+      </main>
 
-        <main style={{ minHeight: "80vh" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/franchise" element={<Franchise />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/branches" element={<Branches />} />
-          </Routes>
-        </main>
-
-        <Footer />
-      </BrowserRouter>
-    </>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
